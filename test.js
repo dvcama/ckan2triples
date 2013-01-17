47c8df600aa500dd9e4b884f6d1b8fd387769c80
Diego Valerio Camarda <diego@lodlive.it>
2013-01-18 00:56:13 +0100

    Revert "second test: dc:title from dat.piemente"
    This reverts commit 4747e3530d87ea83650f4c1c88a6946e4f4850ea.

--- test.js
+++ test.js
@@ -1,23 +1,32 @@
 var http = require('http');
 var fs = require('fs');
+var sys = require('sys');
 var $ = require('jquery');
 console.log('starting!');
 
-/* carico il profilo */
-fs.readFile("profile.js", "UTF-8", function(err, data) {
-	if (err)
-		throw err;
-	start(JSON.parse(data));
+/* lista dei package */
+http.get("http://www.dati.piemonte.it/rpapisrv/api/rest/package", function(res) {
+	// console.log('STATUS: ' + res.statusCode);
+	// console.log('HEADERS: ' + JSON.stringify(res.headers));
+	res.setEncoding('utf8');
+	var resText = "";
+	res.on('data', function(chunk) {
+		resText += chunk;
+	});
+	res.on('end', function() {
+		var resArray = JSON.parse(resText);
+		for ( var int = 0; int < resArray.length && int < 5; int++) {
+			console.log(resArray[int]);
+			singleDataset(resArray[int]);
+		}
+	});
+}).on('error', function(e) {
+	console.log("Got error: " + e.message);
 });
 
-/* si parte! */
-function start(jConfig) {
-	/* eliminiamo l'eventuale file di output precedente */
-	fs.unlink(jConfig.resultFileName, function() {
-	});
-
-	/* lista dei package */
-	http.get(jConfig.packageListUrl, function(res) {
+/* singolo package */
+function singleDataset(dataId) {
+	http.get("http://www.dati.piemonte.it/rpapisrv/api/rest/package/" + dataId, function(res) {
 		// console.log('STATUS: ' + res.statusCode);
 		// console.log('HEADERS: ' + JSON.stringify(res.headers));
 		res.setEncoding('utf8');
@@ -26,52 +35,12 @@ function start(jConfig) {
 			resText += chunk;
 		});
 		res.on('end', function() {
-			var resArray = JSON.parse(resText);
-			for ( var int = 0; int < resArray.length; int++) {
-				singleDataset(resArray[int]);
-			}
+			var resJ = JSON.parse(resText);
+			console.log(resJ.title);
 		});
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
 	});
-
-	/* singolo package */
-	function singleDataset(dataId) {
-		console.log("processing " + jConfig.singlePackageBaseUrl + dataId);
-
-		http.get(jConfig.singlePackageBaseUrl + dataId, function(res) {
-			res.setEncoding('utf8');
-			var resText = "";
-			res.on('data', function(chunk) {
-				resText += chunk;
-			});
-			res.on('end', function() {
-				var resJ = JSON.parse(resText);
-				var map = jConfig.map;
-				$.each(map, function(key, params) {
-					/* genero la uri della nuova risorsa */
-					var resourceUri = res.req.path;
-					if (jConfig.resourceUriSubstitutor && jConfig.resourceUriSubstitutor.replace && jConfig.resourceUriSubstitutor.find) {
-						resourceUri = resourceUri.replace(new RegExp(jConfig.resourceUriSubstitutor.find), jConfig.resourceUriSubstitutor.replace);
-					}
-
-					/* scrivo la tripla */
-					var row = "<"+resourceUri + ">\t" + params.uri + "\t";
-					if (params.type == 'string') {
-						row += '"' + resJ[key] + '"';
-					} else {
-						row += resJ[key];
-					}
-					fs.appendFile(jConfig.resultFileName, row + "\n", function(err) {
-						if (err)
-							throw err;
-					});
-				});
-			});
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
-	}
 }
 
 /*
